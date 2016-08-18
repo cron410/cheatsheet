86c39c455ef85fa573bd3e61147be84616ff50ec
cron410 <cron410@gmail.com>
2016-08-18 10:25:33 -0400

    Create Windows DC.bat

---
+++ NTP/Windows DC.bat
@@ -0,0 +1,10 @@
+sc config netlogon start= auto
+net stop w32time
+w32tm /unregister
+w32tm /register
+net start w32time
+W32tm /config /manualpeerlist:pool.ntp.org,0x1 /syncfromflags:manual /reliable:yes /update
+w32tm /resync /rediscover
+net stop w32time
+net start w32time
+w32tm /resync /rediscover
