d2ddeed71a50e7a943977020d543dd57935cbcae
cron410 <cron410@gmail.com>
2016-08-22 16:57:46 -0400

    Update Windows DC.bat

--- NTP/Windows DC.bat
+++ NTP/Windows DC.bat
@@ -2,6 +2,7 @@ sc config netlogon start= auto
 net stop w32time
 w32tm /unregister
 w32tm /register
+sc config w32time type= own
 net start w32time
 W32tm /config /manualpeerlist:pool.ntp.org,0x1 /syncfromflags:manual /reliable:yes /update
 w32tm /resync /rediscover
