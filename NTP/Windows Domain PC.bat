888edc448aa2430f3826556506ab1a90b35d0185
cron410 <cron410@gmail.com>
2016-08-22 16:58:07 -0400

    Update Windows Domain PC.bat

--- NTP/Windows Domain PC.bat
+++ NTP/Windows Domain PC.bat
@@ -2,6 +2,7 @@ sc config netlogon start= auto
 net stop w32time
 w32tm /unregister
 w32tm /register
+sc config w32time type= own
 net start w32time
 w32tm /config /syncfromflags:domhier /update /reliable:no
 w32tm /resync /rediscover
