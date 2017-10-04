3df6d318d7c5e8ca2402b7a4cc9cfb1ef7ae53b4
cron410 <cron410@gmail.com>
2017-10-04 08:27:37 -0400

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -16,7 +16,7 @@ sed -i 's/^.*PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/ss
 #allow FSCK to auto-fix all errors whenever it runs on boot
 sed -i "s/^FSCKFIX=no$/FSCKFIX=yes/" /etc/default/rcS
 
-#install docker on Debian As ROOT
+#install docker and gluster on Debian As ROOT
 apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
@@ -26,3 +26,11 @@ apt-get update
 apt-get install -y docker-ce
 groupadd docker
 gpasswd -a hive docker
+apt-get install -y python-software-properties
+wget -O - http://download.gluster.org/pub/gluster/glusterfs/LATEST/rsa.pub | apt-key add -
+wget -O - http://download.gluster.org/pub/gluster/glusterfs/3.12/rsa.pub | apt-key add -
+DEBID=$(grep 'VERSION_ID=' /etc/os-release | cut -d '=' -f 2 | tr -d '"')
+DEBVER=$(grep 'VERSION=' /etc/os-release | grep -Eo '[a-z]+')
+echo deb https://download.gluster.org/pub/gluster/glusterfs/LATEST/Debian/${DEBID}/apt ${DEBVER} main > /etc/apt/sources.list.d/gluster.list
+apt-get update
+apt-get install -y glusterfs-server
